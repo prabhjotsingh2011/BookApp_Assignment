9325799e8b167e2caa7180858f7c5701d22422a0
prabhjotsingh2011 <74846094+prabhjotsingh2011@users.noreply.github.com>
2022-01-22 14:12:58 +0530

    Update Tables.js

--- src/views/examples/Tables.js
+++ src/views/examples/Tables.js
@@ -15,6 +15,12 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
+
+
+
+
+
+
 // reactstrap components
 import {
   Badge,
